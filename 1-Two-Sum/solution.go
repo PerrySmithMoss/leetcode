e3a7acf2a9d893e453393550529eda0460235432
Perry Smith-Moss <perrysmithmoss@gmail.com>
2025-02-15 22:04:16 +0000

    1. Two-Sum - Go solution

---
+++ 1-Two-Sum/solution.go
@@ -0,0 +1,13 @@
+package solutions
+
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int, len(nums))
+
+	for i, num := range nums {
+		if value, exists := m[target-num]; exists {
+			return []int{i, value}
+		}
+		m[num] = i
+	}
+	return nil
+}
